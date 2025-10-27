cbc2b3319a70d96fc3ea65923a05007fd65cb206
ibericoder <ibericoder@gmail.com>
2025-10-27 17:44:18 +0100

    hide sommelier chat for now (WIP)=

--- website/src/App.vue
+++ website/src/App.vue
@@ -2,17 +2,11 @@
   <div class="app-container">
     <router-view/>
     <NavigationBar/>
-    
-    <!-- Chat Components -->
-    <ChatToggle />
-    <ChatBot />
   </div>
 </template>
 
 <script setup lang="ts">
 import NavigationBar from './components/NavigationBar.vue';
-import ChatToggle from './components/ChatToggle.vue';
-import ChatBot from './components/ChatBot.vue';
 
 </script>
 <style scoped>
